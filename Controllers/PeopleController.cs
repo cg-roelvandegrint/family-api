b9e09ea9227bfa5969a38cca57952231d7be4f17
Roel van de Grint <roelvandegrint@gmail.com>
2021-08-23 22:37:38 +0200

    Initial commit

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Family.Api.Models;
+
+namespace Family.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public partial class PeopleController : ControllerBase
+    {
+        private static readonly IEnumerable<Person> People = new List<Person>
+        {
+            new Person("Roel", "van de Grint"),
+            new Person("Mandy","Dirkse"),
+            new Person("May","van de Grint"),
+            new Person("Fenn","van de Grint")
+                    };
+
+        private readonly ILogger<PeopleController> _logger;
+
+        public PeopleController(ILogger<PeopleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return People;
+        }
+    }
+}
---
+++ Controllers/Person.cs
@@ -0,0 +1,15 @@
+namespace Family.Api.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
